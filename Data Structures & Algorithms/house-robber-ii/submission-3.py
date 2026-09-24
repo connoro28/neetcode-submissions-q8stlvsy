97163b38cb7604598a88bd2085b20bb19e3ca27e
connoro28 <75001569+connoro28@users.noreply.github.com>
2026-09-24 17:48:50 -0400

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def robline(arr):
+            memo = {}
+            def dfs(i):
+                if i >= len(arr):
+                    return 0
+                if i in memo:
+                    return memo[i]
+                memo[i] = max(arr[i]+dfs(i+2), dfs(i+1))
+                return memo[i]
+            return dfs(0)
+        return max(robline(nums[1:]), robline(nums[:-1]))
