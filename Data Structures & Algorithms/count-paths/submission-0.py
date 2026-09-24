71c14d1336267c41bc21ebec4e55e21999736f5a
connoro28 <75001569+connoro28@users.noreply.github.com>
2026-09-24 19:52:09 -0400

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {}
+        def dp(r, c):
+            if r >= m or c >= n:
+                return 0
+            if r == m - 1 or c == n - 1:
+                return 1
+            if (r, c) in memo:
+                return memo[(r,c)]
+            memo[(r,c)] = dp(r+1,c) + dp(r,c+1)
+            return memo[(r,c)]
+        return dp(0,0)
+
+        
